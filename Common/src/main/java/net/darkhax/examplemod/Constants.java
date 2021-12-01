66080900321ca90a4cc3ab2777befe789762b29e
Tyler Hancock <darklime@live.ca>
2021-12-01 01:19:22 -0700

    Groundwork for MultiLoader support.

---
+++ Common/src/main/java/net/darkhax/examplemod/Constants.java
@@ -0,0 +1,11 @@
+package net.darkhax.examplemod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Constants {
+
+	public static final String MOD_ID = "examplemod";
+	public static final String MOD_NAME = "ExampleMod";
+	public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+}

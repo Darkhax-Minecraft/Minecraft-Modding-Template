73e3af82f503f1bc0f9b0ba071e74d9c20b992ed
Tyler Hancock <darklime@live.ca>
2021-12-04 00:14:18 -0700

    Implement support for Mixins on both sides.

--- Common/src/main/java/net/darkhax/examplemod/mixin/ExampleMixin.java
+++ Common/src/main/java/net/darkhax/examplemod/mixin/ExampleMixin.java
@@ -14,7 +14,7 @@ public class ExampleMixin {
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
         
-        Constants.LOG.info("This line is printed by an example mod mixin from Fabric!");
+        Constants.LOG.info("This line is printed by an example mod mixin!");
         Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
         Constants.LOG.info("Classloader: {}", this.getClass().getClassLoader());
     }
